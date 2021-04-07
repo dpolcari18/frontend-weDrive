13f12fb854fd09739f254ec35f1e0e93598602e0
David Polcari <dpolcari18@gmail.com>
2021-04-07 14:59:09 -0500

    login stores auth key in local storage

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,7 +8,10 @@ const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    const handleSubmit = async () => {
+    // send fetch to rails api to create session
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
         const user = {
             user: {
                 email: email,
@@ -25,13 +28,24 @@ const Login = () => {
         }
 
         const postSes = await fetch(LOGIN_URL, userObj)
+        const sesRes = await postSes.json()
+
+        if (sesRes.status === 'Success') {
+            localStorage.setItem('auth_key', sesRes.token)
+        } else if (sesRes.status === 'Failed') {
+            alert(sesRes.msg)
+            // Reset form if login fails
+            setEmail('')
+            setPassword('')
+        }
+        
     }
 
     return (
         <div>
             <h3>LOGIN</h3>
             <div>
-                <form onSubmit={() => handleSubmit()}>
+                <form onSubmit={(e) => handleSubmit(e)}>
                     <div>
                         <input type='email' name='email' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}></input>
                     </div>
