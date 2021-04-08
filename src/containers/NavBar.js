fd1ff8431286f278779fcf6a803a3f3463e627ec
David Polcari <dpolcari18@gmail.com>
2021-04-08 11:33:52 -0500

    navbar renders conditionally based on loggedIn and is fixed to top

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -3,7 +3,7 @@ import { withRouter } from 'react-router'
 import { Link } from 'react-router-dom'
 
 // Redux 
-import { connect } from 'react-redux'
+import { connect, Provider } from 'react-redux'
 
 // React-Bootstrap
 import Navbar from 'react-bootstrap/Navbar'
@@ -11,23 +11,41 @@ import Nav from 'react-bootstrap/Nav'
 
 const NavBar = ({ history, logout_user, loggedIn }) => {
 
+    // Logout user
     const handleLogout = () => {
         localStorage.removeItem('auth_key')
         logout_user()
         history.push('/')
     }
 
+    // Conditionally render navbar based on loggedIn
+    const checkLoggedIn = () => {
+        if (loggedIn) {
+            return (
+                <Navbar bg="light" variant="light" fixed='top'>
+                    <Navbar.Brand><Link to='/home'>weDrive</ Link></Navbar.Brand>
+                    <Nav className="mr-auto">
+                        <Nav.Link><Link to='/home'>Home</Link></Nav.Link>
+                        <Nav.Link><Link to='/profile'>Profile</Link></Nav.Link>
+                        <Nav.Link><Link to='trips'>Trips</Link></Nav.Link>
+                        <Nav.Link><Link to='vehicles'>Vehicles</Link></Nav.Link>
+                        <Nav.Link onClick={() => handleLogout()}>Logout</Nav.Link>
+                    </Nav>
+                </Navbar>
+            ) 
+        } else {
+            return(
+                <Navbar bg="light" variant="light">
+                    <Navbar.Brand><Link to='/home'>weDrive</ Link></Navbar.Brand>
+                </Navbar>
+            )
+        }
+    }
+
     return (
-        <Navbar bg="light" variant="light">
-            <Navbar.Brand><Link to='/home'>weDrive</ Link></Navbar.Brand>
-            <Nav className="mr-auto">
-                <Nav.Link><Link to='/home'>Home</Link></Nav.Link>
-                <Nav.Link><Link to='/profile'>Profile</Link></Nav.Link>
-                <Nav.Link><Link to='trips'>Trips</Link></Nav.Link>
-                <Nav.Link><Link to='vehicles'>Vehicles</Link></Nav.Link>
-                <Nav.Link onClick={() => handleLogout()}>Logout</Nav.Link>
-            </Nav>
-        </Navbar>
+       <div>
+           {checkLoggedIn()}
+       </div>
     )
 }
 
